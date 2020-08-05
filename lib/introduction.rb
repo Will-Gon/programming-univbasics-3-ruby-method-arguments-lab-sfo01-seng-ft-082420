e7392a95f11cd54149c1724cbb2ef90a5f6b070a
Will Gonzalez <will.colocho@gmail.com>
2020-08-05 06:01:08 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,7 @@ def introduction(name)
 end
 introduction("Will")
 
-introduction_with_language(name, language)
+def introduction_with_language(name, language)
 puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 introduction_with_language("Will", "Ruby")
